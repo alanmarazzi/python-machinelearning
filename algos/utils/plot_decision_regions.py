b7594f81a8af65646954b160fff276677a2862d7
alan <alan.marazzi@leitha.eu>
2018-02-16 17:22:24 +0100

    Update Perceptron

--- algos/utils/plot_decision_regions.py
+++ algos/utils/plot_decision_regions.py
@@ -2,7 +2,7 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+def plot_decision_regions(X, y, classifier, figsize=(10, 6), resolution=0.02):
     markers = ("s", "x", "o", "^", "v")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[:len(np.unique(y))])
@@ -11,12 +11,15 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.figure(figsize=figsize)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
-
-    if test_idx:
-        X_test, y_test = X[test_idx, :], y[test_idx]
-        plt.scatter(X_test[:, 0], X_test[:, 1], c = "", alpha = 1.0, linewidths=1, marker='o', s=55, label="test set")
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1], 
+                    alpha=0.8, 
+                    c=cmap(idx), 
+                    marker=markers[idx], 
+                    label=cl,
+                    edgecolor='black')
